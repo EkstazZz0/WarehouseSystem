42d2b33440da075f9e5e606eba1c9cc252b558db
EkstazZz0 <128053305+EkstazZz0@users.noreply.github.com>
2025-04-03 11:17:32 +0400

    fixed with loading_dotenv

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db_connect_url = os.getenv("DATABASE_CONNECT_URL")
 
--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 
 from app.db.repository import init_db, drop_db
 from app.api.v1.endpoints.items import router as item_router
 from app.api.v1.endpoints.orders import router as order_router
 
-load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
